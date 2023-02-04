725f9d335607a78c40b4d9332eb66a38ad4f0da7
Randall-Eckstein <randalleckstein@yahoo.com>
2023-02-03 19:17:28 -0700

    Added content and validated HTML and CSS

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,4 +1,8 @@
 let date = new Date();
+
+// for testing purposes: uncomment and select day of the month to test banner:
+// date = new Date(2023, 1, 7);
+
 document.querySelector(".year").textContent = date.getFullYear();
 document.querySelector(".update").textContent = document.lastModified;
 const fullDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(date);
@@ -12,3 +16,10 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
+const banner = document.querySelector(".banner");
+if (date.getDay() == 2 || date.getDay() == 3){
+    banner.style.display = "block";
+}
+else {
+    banner.style.display = "none";
+}

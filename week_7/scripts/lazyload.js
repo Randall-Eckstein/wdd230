974a4613547224e4705b9cac8eb79eb030548e01
Randall-Eckstein <randalleckstein@yahoo.com>
2023-02-13 20:45:26 -0700

    added a console log line to see when images load

--- week_7/scripts/lazyload.js
+++ week_7/scripts/lazyload.js
@@ -6,14 +6,11 @@ const loadImages = (image) => {
     };
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
-
 if ("IntersectionObserver" in window) {
     const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
             if (item.isIntersecting) {
+                console.log("Seeing an image")
                 loadImages(item.target);
                 observer.unobserve(item.target);
             }
